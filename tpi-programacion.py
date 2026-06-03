1557b5f518ddb1616305cc37b940c92a617226e8
LucasBarragan <lucasbarragan000@gmail.com>
2026-06-02 21:23:22 -0300

    Opcion 1 (agregar pais), carga de paises desde CSV y menu principal

--- tpi-programacion.py
+++ tpi-programacion.py
@@ -23,12 +23,71 @@ def cargar_paises_csv():
 
     return paises
 
-
-# Prueba
-
 paises = cargar_paises_csv()
 
-print(f"Se cargaron {len(paises)} países.")
+# AGREGAR PAIS
 
-for pais in paises:
-    print(pais)
+def agregar_pais(paises):
+
+    nombre = input("Ingrese el nombre del país: ").capitalize()
+    poblacion = int(input("Ingrese la población: "))
+    superficie = int(input("Ingrese la superficie: "))
+    continente = input("Ingrese el continente: ").capitalize()
+
+    nuevo_pais = {
+        "nombre": nombre,
+        "poblacion": poblacion,
+        "superficie": superficie,
+        "continente": continente
+    }
+
+    paises.append(nuevo_pais)
+
+    print("País agregado correctamente.")
+    #print(paises[-1]) probamos si funciona
+
+# MENU
+
+def mostrar_menu():
+    print("\n=== GESTIÓN DE PAÍSES ===")
+    print("1. Agregar país")
+    print("2. Actualizar país")
+    print("3. Buscar país")
+    print("4. Filtrar países")
+    print("5. Ordenar países")
+    print("6. Mostrar estadísticas")
+    print("0. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    return opcion
+
+
+
+while True:
+    opcion = mostrar_menu()
+
+    if opcion == "1":
+        agregar_pais(paises)
+
+    elif opcion == "2":
+        print("Actualizar país")
+
+    elif opcion == "3":
+        print("Buscar país")
+
+    elif opcion == "4":
+        print("Filtrar países")
+
+    elif opcion == "5":
+        print("Ordenar países")
+
+    elif opcion == "6":
+        print("Mostrar estadísticas")
+
+    elif opcion == "0":
+        print("Programa finalizado.")
+        break
+
+    else:
+        print("Opción inválida.")
