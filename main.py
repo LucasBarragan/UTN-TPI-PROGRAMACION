733ac4dcdd4b1478d242d63b6495f21e0da59eaf
lorenzomoreyra99-cpu <lorenzomoreyra99@gmail.com>
2026-06-13 11:09:05 -0300

    modularizamos cargar paises

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-# CARGA DE PAISES
-def cargar_paises_csv():
-    paises = []
-
-    archivo = open("paises.csv", "r")
-
-    # Saltar encabezado
-    archivo.readline()
-
-    for linea in archivo:
-        linea = linea.strip()
-        datos = linea.split(",")
-
-        pais = {
-            "nombre": datos[0],
-            "poblacion": int(datos[1]),
-            "superficie": int(datos[2]),
-            "continente": datos[3]
-        }
-
-        paises.append(pais)
-
-    archivo.close()
-
-    return paises
-
+from archivo import cargar_paises_csv
 paises = cargar_paises_csv()
 
 # AGREGAR PAIS (1)
