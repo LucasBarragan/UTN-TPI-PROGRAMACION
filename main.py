4b860a26c86c70c6720cad3fcd6befc675194735
lorenzomoreyra99-cpu <lorenzomoreyra99@gmail.com>
2026-06-13 11:15:38 -0300

    creamos el modulo paises

--- main.py
+++ main.py
@@ -1,120 +1,17 @@
 from archivo import cargar_paises_csv
+
+from paises import(
+    agregar_pais,
+    actualizar_pais,
+    buscar_pais
+)
+
 paises = cargar_paises_csv()
 
-# AGREGAR PAIS (1)
 
-def agregar_pais(paises):
 
-    # Validar nombre
-    while True:
-        nombre = input("Ingrese el nombre del país: ").strip().lower()
-        if nombre != "":
-            break
-        print("El nombre no puede estar vacío.")
 
-    # Verificar si ya existe
-    for pais in paises:
-        if pais["nombre"] == nombre:
-            print("Ese país ya existe.")
-            return
 
-    # Validar población
-    while True:
-        try:
-            poblacion = int(input("Ingrese la población: "))
-            if poblacion > 0:
-                break
-            print("La población debe ser mayor que 0.")
-        except ValueError:
-            print("Debe ingresar un número.")
-
-    # Validar superficie
-    while True:
-        try:
-            superficie = int(input("Ingrese la superficie: "))
-            if superficie > 0:
-                break
-            print("La superficie debe ser mayor que 0.")
-        except ValueError:
-            print("Debe ingresar un número.")
-
-    # Validar continente
-    while True:
-        continente = input("Ingrese el continente: ").strip().lower()
-        if continente != "":
-            break
-        print("El continente no puede estar vacío.")
-
-    nuevo_pais = {
-        "nombre": nombre,
-        "poblacion": poblacion,
-        "superficie": superficie,
-        "continente": continente
-    }
-
-    paises.append(nuevo_pais)
-
-    print("País agregado correctamente.")
-
-# ACTUALIZAR PAISES (2)
-
-def actualizar_pais(paises):
-
-    while True:
-        nombre_buscado = input("Ingrese el nombre del país a actualizar: ").strip()
-        if nombre_buscado != "":
-            break
-        print("Debe ingresar un nombre.")
-    encontrado = False
-
-    for pais in paises:
-        if nombre_buscado.lower() == pais["nombre"].lower():
-            print("\nPaís encontrado:")
-            print(pais)
-            # Nueva población
-            while True:
-                try:
-                    nueva_poblacion = int(input("Ingrese la nueva población: "))
-                    if nueva_poblacion > 0:
-                        break
-                    print("La población debe ser mayor que 0.")
-                except ValueError:
-                    print("Debe ingresar un número.")
-            # Nueva superficie
-            while True:
-                try:
-                    nueva_superficie = int(input("Ingrese la nueva superficie: "))
-                    if nueva_superficie > 0:
-                        break
-                    print("La superficie debe ser mayor que 0.")
-                except ValueError:
-                    print("Debe ingresar un número.")
-            pais["poblacion"] = nueva_poblacion
-            pais["superficie"] = nueva_superficie
-            print("\nPaís actualizado correctamente.")
-            print(pais)
-            encontrado = True
-            break
-
-    if not encontrado:
-        print("No se encontró el país.")
-
-# BUSCAR PAIS (3)
-
-def buscar_pais(paises):
-    while True:
-        nombre_buscado = input("Ingrese el nombre del país: ").strip()
-        if nombre_buscado != "":
-            break
-        print("Debe ingresar un nombre.")
-    encontrado = False
-    for pais in paises:
-        if nombre_buscado.lower() in pais["nombre"].lower():
-            print("\nPaís encontrado:")
-            print(pais)
-            encontrado = True
-    if not encontrado:
-        print("No se encontraron países.")
 
 # FILTRAR PAISES (4)
 
